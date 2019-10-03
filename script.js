793719bf2471ba83132e3f23a6dd9b87d8728443
Katie McBride <mcbride.katieb@gmail.com>
2019-10-03 18:19:17 -0500

    startQuiz function

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
+
 // Set variables
-
-var startQuiz = document.getElementById("#start");
-
-var el = document.querySelector("#start");
-if(el){
-    console.log("its working");
-  };
-
-
+    var timeLeft = 60;
+    var timeClock = document.querySelector("#timer");
 
 // Function startQuiz 
-    // Timer starts
+    document.querySelector("#startBtn").addEventListener('click',startQuiz);
 
+function startQuiz() {
+// Timer starts
+    var timerInterval = setInterval(function() {
+        timeLeft--;
+        timeClock.textContent = timeLeft + " seconds left for quiz.";
+        startQuestions();
+          
+        if(timeLeft === 0) {
+        clearInterval(timerInterval);
+        }
 
-    // Quiz questions pop up
+    }, 1000);
 
-// Answering right or wrong
+}
+
+// Quiz questions pop up
+    function startQuestions() {
+      
+    var questionsEl = document.createElement("questionsEl");
+      
+    imgEl.setAttribute("src", "images/image_1.jpg");
+    mainEl.appendChild(imgEl);
+      
+};
+
+    // Answering right or wrong
 
     // Take off 15 seconds of time
 
-//If timer runs out OR questions finished time stops
+    //If timer runs out OR questions finished time stops
 
-//Call to action quizStop
+    //Call to action quizStop
 
     // Prompt to put in initials
 
     // Calculate score
 
-// Put score into leaderboard 'localstorage'
+    // Put score into leaderboard 'localstorage'
 
 
 
 // Call to action 'Start Quiz' by click of button
-
