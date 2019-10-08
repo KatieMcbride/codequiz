1e8398d46a407aac6009490fbc6cb7f315b355cd
Katie McBride <mcbride.katieb@gmail.com>
2019-10-07 21:52:12 -0500

    questions appear

--- questions.js
+++ questions.js
@@ -1,30 +1,33 @@
 // Store quiz questions here. Example below
 
-var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    
+var questionArr = [
+   
+  {
+  question: "The condition in an if / else statement is enclosed within ____.",
+  answerOne: "quotes",
+  answerTwo: "curly brackets",
+  answerThree: "parentheses"
+  }
   ];
+    {
+      // question: "The condition in an if / else statement is enclosed within ____.",
+      // choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      // answer: "parentheses"
+    },
+  //   {
+  //     question: "The condition in an if / else statement is enclosed within ____.",
+  //     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  //     answer: "parentheses"
+  //   },
+  //   {
+  //     question: "The condition in an if / else statement is enclosed within ____.",
+  //     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  //     answer: "parentheses"
+  //   },
+  //   {
+  //     question: "The condition in an if / else statement is enclosed within ____.",
+  //     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  //     answer: "parentheses"
+  //   },
+    
+  // ];
--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 // Set variables
     var timeLeft = 60;
     var timeClock = document.querySelector("#timer");
-    var score = 0;
+    var correct = 0;
+    var incorrect = 0;
 
 // Function startQuiz 
     document.querySelector("#startBtn").addEventListener('click',startQuiz);
@@ -12,28 +13,80 @@ function startQuiz() {
     var timerInterval = setInterval(function() {
         timeLeft--;
         timeClock.textContent = timeLeft + " seconds left for quiz.";
+        // startQuestions()
         
           
         if(timeLeft === 0) {
         clearInterval(timerInterval);
-        startQuestions();
         }
 
     }, 1000);
 
 }
 
-// Quiz questions pop up
-    function startQuestions() {
+var questionArr = [
+   
+    {
+    question: "The condition in an if / else statement is enclosed within ____.",
+    answerOne: "quotes",
+    answerTwo: "curly brackets",
+    answerThree: "parentheses"
+    }  
+    ];
+    
+    function buttonFunction(){
+        for(var i = 0; i < questionArr.length; i += 1){
+    
+        function getQuestion(){
+            var questionInner = document.getElementById("question");
+            return questionInner.innerHTML = questionArr[i].question;
+        }
+        getQuestion();
+    
+        function getAnswerOne(){
+            var answerOneInner = document.getElementById("answerOne");
+            return answerOneInner.innerHTML = questionArr[i].answerOne;
+        }
+        getAnswerOne();
+    
+    
+        function getAnswerTwo(){
+            var answerTwoInner = document.getElementById("answerTwo");
+            return answerTwoInner.innerHTML = questionArr[i].answerTwo;
+        }
+        getAnswerTwo();
+    
+        function getAnswerThree(){
+            var answerThreeInner = document.getElementById("answerThree");
+            return answerThreeInner.innerHTML = questionArr[i].answerThree;
+        }
+        getAnswerThree();
+      }
+
+    }
+
+
+// // Quiz questions pop up
+// function startQuestions() {
+//     var quizArea = document.querySelector("#quiz");  
+//     var questionsEl = document.createElement("questionsEl");
+
+//     // HOW DO I GET QUESTION TO POP UP???  
+//     questionsEl.setAttribute("href", "./questions.js");
+//     quizArea.appendChild(questionsEl);
+
+//     for (var i=0; i < questions.length; i++ ){
+//         prompt(questions);
+           
+//         };
       
-    var questionsEl = document.createElement("questionsEl");
+
+// };
       
-    imgEl.setAttribute("src", "images/image_1.jpg");
-    mainEl.appendChild(imgEl);
-      
-};
 
     // Answering right or wrong
+
+
 
     // Take off 15 seconds of time
 
