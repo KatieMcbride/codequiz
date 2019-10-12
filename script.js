2572361aa0b1bb789c3fb6a3f624ed1502c38085
Katie McBride <mcbride.katieb@gmail.com>
2019-10-12 15:53:57 -0500

    local storage

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 // Set variables
-var timeLeft = 2;
+var timeLeft = 60;
+var timerInterval = 0;
 var timeClock = document.querySelector("#timer");
 var score = 0;
 // var questionIndex = 0;
@@ -13,27 +14,37 @@ var quizBox = document.getElementById("quizBox");
 var leaderScore = document.getElementById("leaderScore");
 var leaders = [];
 var leaderList = document.getElementById("leader-list");
-var scoreBoardForm = document.getElementById("score-board");
+var submitButton = document.getElementById("submit-button");
 var scoreInput = document.getElementById("score-text");
 
 init();
 
 function init(){
     leaderScore.style.display = "none";
-}
+    //  Get stored leaders from localStorage
+    //     Parsing the JSON string to an object
+    var lastUser = JSON.parse(localStorage.getItem("leaderHighScore"));
+    console.log(lastUser);
+    // If todos were retrieved from localStorage, update the todos array to it
+    if (lastUser !== null) {
+      leaders = lastUser;
+    }
+    console.log(lastUser);
+   }
+ // }
 
 // Call to action 'Start Quiz' by click of button
 document.querySelector("#startBtn").addEventListener('click',startQuiz);
 
 function startQuiz() {
     // Timer starts    
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
         timeLeft--;
         timeClock.textContent = timeLeft + " seconds left for quiz.";
         renderQuestions();
        
         if(timeLeft === 0) {
-        clearInterval(timerInterval);
+
         leaderBoard();
         }
 
@@ -50,23 +61,25 @@ function renderQuestions(){
 
 document.getElementById("nextButton").addEventListener("click", function(){
     console.log(i);
+     //If timer runs out OR questions finished time stops
     if (i > lastQuestionIndex){
+        clearInterval(timerInterval);
         leaderBoard();
      }  else {
         renderQuestions();
         i++;
-   
 }
 });
 
 
     function checkAnswer(answer){
         if (answer === questionArr[i].correct) {
-            console.log(++score);
+            timeLeft +=10;
         } if (answer !== questionArr[i].correct){
             // decrease 15 seconds of time
             timeLeft -=15;
         } 
+        // yourscore.textContent = timeLeft;
     }
 
     function leaderBoard(){
@@ -82,25 +95,65 @@ document.getElementById("nextButton").addEventListener("click", function(){
         }
     }
 
-    scoreBoardForm.addEventListener("submit", function(event) {
+    submitButton.addEventListener("click", function(event) {
         event.preventDefault();
+        storeScore();
            
-        var scoreText = scoreInput.value.trim();
+        // var scoreText = scoreInput.value.trim();
   
-        // // Return from function early if submitted todoText is blank
+        // // Return from function early if submitted scoreText is blank
         // if (scoreText === "") {
         // return;
         // }
   
-        // Add new todoText to todos array, clear the input
-        leaders.push(scoreText);
-        scoreInput.value = "";
-  
+        // // Add new scoreText to leader array, clear the input
+        // leaders.push(scoreText);
+        // scoreInput.value = "";
+
+        
+        storeLeaders();
         // Re-render the list
+
         leaderBoard();
     });
-  
- 
+
+    function storeScore(event) {
+        leaders[leaders.length] = {
+            names: scoreInput.value,
+            savedScores: timeLeft
+        }
+    }        
+
+   
+    function showScore(){
+        var lastUser = JSON.parse(localStorage.getItem("leaderHighScore"));
+        // for (var i = 0; i < lastUser.length; i++) {
+        //     console.log("last user" + lastUser[i].savedScores);
+        //     var name = user[i].names;
+        //     var score = user[i].savedScores;
+        //     var div = document.createElement("div");
+        //     div.textContent = name + " " + score;
+        //     div.setAttribute("data-index", i);
+        //     leader-list.appendChild(div);
+        //   }
+         }
+    }
+        
+
+    function storeLeaders() {
+        // Stringify and set "scores" key in localStorage to leader array
+        localStorage.setItem("leaderHighScore", JSON.stringify(leaders));
+
+      }
+
+    //   leader[leader.length] = {
+    //     names: scoreInput.value,
+    //     savedScores: score
+
+    //     localStorage.setItem("leaderHighScore"), JSON.stringify(timerInterval));
+    // }
+
+    
     
 
 
@@ -112,34 +165,8 @@ document.getElementById("nextButton").addEventListener("click", function(){
 // Highscore Link
       
 
-
-    // Calculate score
-
-    //If timer runs out OR questions finished time stops
-
     //Call to action quizStop or Submit button to showResults
 
-    // Prompt to put in initials when submit button
-    // todoForm.addEventListener("submit", function(event) {
-    //     event.preventDefault();
-      
-    //     var todoText = todoInput.value.trim();
-      
-    //     // Return from function early if submitted todoText is blank
-    //     if (todoText === "") {
-    //       return;
-    //     }
-      
-    //     // Add new todoText to todos array, clear the input
-    //     todos.push(todoText);
-    //     todoInput.value = "";
-      
-    //     // Store updated todos in localStorage, re-render the list
-    //     storeTodos();
-    //     renderTodos();
-    //   });
-
-    
 
 
 
